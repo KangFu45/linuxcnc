dc105247590d0460750d868a4a1f11b48f638738
KangFu45 <32917005+KangFu45@users.noreply.github.com>
2020-05-19 09:15:33 +0800

    three axis AM

--- share/qtvcp/screens/qtdragon_old/qtdragon_old_handler.py
+++ share/qtvcp/screens/qtdragon_old/qtdragon_old_handler.py
@@ -5,6 +5,7 @@ import hal
 from PyQt5 import QtCore, QtWidgets
 from qtvcp.widgets.mdi_line import MDILine as MDI_WIDGET
 from qtvcp.widgets.gcode_editor import GcodeEditor as GCODE
+from qtvcp.widgets.tool_offsetview import ToolOffsetView as TOOL_TABLE
 from qtvcp.widgets.origin_offsetview import OriginOffsetView as OFFSET_VIEW
 from qtvcp.lib.keybindings import Keylookup
 from qtvcp.core import Status, Action, Info
@@ -29,16 +30,19 @@ class HandlerClass:
         self.STYLEEDITOR = SSE(widgets,paths)
         KEYBIND.add_call('Key_F12','on_keycall_F12')
         
+#        STAT.connect('general',self.return_value)
         STAT.connect('state-on', self.machine_on)
         STAT.connect('state-off', self.machine_off)
+        STAT.connect('gcode-line-selected', self.gcode_line_selected)
         STAT.connect('hard-limits-tripped', self.hard_limit_tripped)
         STAT.connect("interp-idle", self.interp_idle_changed)
         STAT.connect("user-system-changed", self.user_system_changed)
+        STAT.connect("tool-in-spindle-changed", self.tool_in_spindle_changed)
         STAT.connect("file-loaded", self.file_loaded)
         STAT.connect("all-homed", self.all_homed)
         STAT.connect("not-all-homed", self.not_homed)
 
-        # some global variables
+# some global variables
         self.axis_list = INFO.AVAILABLE_AXES
         self.joint_list = INFO.AVAILABLE_JOINTS
         self.max_velocity = INFO.MAX_LINEAR_VELOCITY
@@ -46,16 +50,21 @@ class HandlerClass:
         self.home_location_x = self.inifile.find('JOINT_0', 'HOME')
         self.home_location_y = self.inifile.find('JOINT_1', 'HOME')
         self.home_location_z = self.inifile.find('JOINT_2', 'HOME')
+        self.tool_sensor_x = self.inifile.find('TOOLSENSOR', 'X')
+        self.tool_sensor_y = self.inifile.find('TOOLSENSOR', 'Y')
+        self.laser_offset_x = self.inifile.find('LASER', 'X')
+        self.laser_offset_y = self.inifile.find('LASER', 'Y')
         self.homed = False
         self.start_line = 0
         self.program_length = 0
         self.slow_jog_factor = 10
+        self.tool_in_spindle = 0
         self.reload_tool = 0
         self.last_loaded_program = ""
         self.onoff_list = ["widget_controls", "Program_frame", "DRO_frame"]
 
     def initialized__(self):
-        #self.init_pins()
+        self.init_pins()
         self.init_preferences()
         self.init_widgets()
         self.init_locations()
@@ -67,59 +76,31 @@ class HandlerClass:
     #############################
     # SPECIAL FUNCTIONS SECTION #
     #############################
-    #def init_pins(self):
+    def init_pins(self):
         # these pins are needed so that the touchoff subroutine can read the variables
-        #self.hal.newpin("touch_height", hal.HAL_FLOAT, hal.HAL_OUT)
+        self.hal.newpin("touch_height", hal.HAL_FLOAT, hal.HAL_OUT)
+        self.hal.newpin("sensor_height", hal.HAL_FLOAT, hal.HAL_OUT)
+        self.hal.newpin("zero_height", hal.HAL_FLOAT, hal.HAL_OUT)
+        self.hal.newpin("search_vel", hal.HAL_FLOAT, hal.HAL_OUT)
+        self.hal.newpin("probe_vel", hal.HAL_FLOAT, hal.HAL_OUT)
+        self.hal.newpin("max_probe", hal.HAL_FLOAT, hal.HAL_OUT)
         
     def init_preferences(self):
         if self.w.PREFS_:
+            self.reload_tool = self.w.PREFS_.getpref('Tool to load', 0, int,'CUSTOM_FORM_ENTRIES')
             self.last_loaded_program = self.w.PREFS_.getpref('last_file_path', None, str,'BOOK_KEEPING')
             temp1 = self.w.PREFS_.getpref('Reload program', False, bool,'CUSTOM_FORM_ENTRIES')
+            temp2 = self.w.PREFS_.getpref('Reload tool', False, bool,'CUSTOM_FORM_ENTRIES')
+            temp3 = self.w.PREFS_.getpref('Tool sensor', False, bool,'CUSTOM_FORM_ENTRIES')
         else:
-            temp1 = False
+            temp1 = temp2 = temp3 = False
             self.add_alarm("No preference file found")
         self.w.checkBox_reload_program.setChecked(temp1)
+        self.w.checkBox_reload_tool.setChecked(temp2)
+        self.w.checkBox_tool_sensor.setChecked(temp3)
+        self.chk_tool_sensor(temp3)
 
     def init_widgets(self):
-        #laser power set
-        MAX_POWER = int(self.inifile.find('LASER', 'POWER')) or 500
-        FIX_VEL_1 = int(self.inifile.find('LASER', 'FIX_VEL_1')) or 100
-        FIX_VEL_2 = int(self.inifile.find('LASER', 'FIX_VEL_2')) or 200
-        FIX_VEL_3 = int(self.inifile.find('LASER', 'FIX_VEL_3')) or 400
-        self.w.label_max_power.setText(str(MAX_POWER))
-        self.w.slider_power.setMaximum(MAX_POWER)
-        self.w.slider_power.setValue(MAX_POWER/2)
-        compare = lambda v,min,max : (v>min) and (v<max)
-        if compare(FIX_VEL_1,0,MAX_POWER) and compare(FIX_VEL_2,0,MAX_POWER) and compare(FIX_VEL_3,0,MAX_POWER):
-            self.w.btn_power_1.setText(str(FIX_VEL_1))
-            self.w.btn_power_2.setText(str(FIX_VEL_2))
-            self.w.btn_power_3.setText(str(FIX_VEL_3))
-        else:
-            self.w.btn_power_1.setText(str(0))
-            self.w.btn_power_2.setText(str(MAX_POWER/2))
-            self.w.btn_power_3.setText(str(MAX_POWER))
-        #powder vel set
-        MAX_POWER = int(self.inifile.find('POWDER', 'MAX_VEL')) or 29
-        FIX_VEL_1 = float(self.inifile.find('POWDER', 'FIX_VEL_1')) or 1
-        FIX_VEL_2 = float(self.inifile.find('POWDER', 'FIX_VEL_2')) or 5
-        FIX_VEL_3 = float(self.inifile.find('POWDER', 'FIX_VEL_3')) or 10
-        POWDER_NUM = int(self.inifile.find('POWDER', 'NUM')) or 1
-        if POWDER_NUM > 26:
-            POWDER_NUM = 26
-        self.w.label_max_powder_vel.setText(str(MAX_POWER))
-        self.w.slider_powder_vel.setMaximum(MAX_POWER*10)
-        self.w.slider_powder_vel.setValue(MAX_POWER / 2)
-        if compare(FIX_VEL_1, 0, MAX_POWER) and compare(FIX_VEL_2, 0, MAX_POWER) and compare(FIX_VEL_3, 0,MAX_POWER):
-            self.w.btn_powder_vel_1.setText(str(FIX_VEL_1))
-            self.w.btn_powder_vel_2.setText(str(FIX_VEL_2))
-            self.w.btn_powder_vel_3.setText(str(FIX_VEL_3))
-        else:
-            self.w.btn_powder_vel_1.setText(str(0))
-            self.w.btn_powder_vel_2.setText(str(MAX_POWER / 2))
-            self.w.btn_powder_vel_3.setText(str(MAX_POWER))
-        for index in range(POWDER_NUM):
-            self.w.comboBox_powder_num.addItem("PDR " + str(index + 1))
-        #other set
         self.w.main_tab_widget.setCurrentIndex(0)
         self.w.slider_jog.setMaximum(self.max_velocity * 60)
         self.w.slider_jog.setValue(INFO.DEFAULT_LINEAR_JOG_VEL)
@@ -129,6 +110,9 @@ class HandlerClass:
         self.w.slider_feed.setValue(100)
         self.w.slider_rapid.setMaximum(100)
         self.w.slider_rapid.setValue(100)
+        self.w.slider_spindle.setMinimum(INFO.MIN_SPINDLE_OVERRIDE)
+        self.w.slider_spindle.setMaximum(INFO.MAX_SPINDLE_OVERRIDE)
+        self.w.slider_spindle.setValue(100)
         self.w.checkBox_override_limits.setChecked(False)
         self.w.checkBox_override_limits.setEnabled(False)
         self.w.filemanager.show()
@@ -136,7 +120,22 @@ class HandlerClass:
         self.w.btn_from_line.setEnabled(False)
        
     def init_locations(self):
+        touch_height = self.inifile.find('TOOLSENSOR', 'TOUCH_HEIGHT') or "50"
+        sensor_height = self.inifile.find('TOOLSENSOR', 'SENSOR_HEIGHT') or "50"
+        max_probe = self.inifile.find('TOOLSENSOR', 'MAXPROBE') or "40"
+        search_vel = self.inifile.find('TOOLSENSOR', 'SEARCH_VEL') or "200"
+        probe_vel = self.inifile.find('TOOLSENSOR', 'PROBE_VEL') or "50"
         self.w.lbl_maxv.setText(str(self.max_velocity * 60))
+        self.w.lbl_touch_height.setText(touch_height)
+        self.w.lbl_sensor_height.setText(sensor_height)
+        self.w.lbl_maxprobe.setText(max_probe)
+        self.w.lbl_search_vel.setText(search_vel)
+        self.w.lbl_probe_vel.setText(probe_vel)
+        self.hal['touch_height'] = touch_height
+        self.hal['sensor_height'] = sensor_height
+        self.hal['max_probe'] = max_probe
+        self.hal['search_vel'] = search_vel
+        self.hal['probe_vel'] = probe_vel
         # home location
         if not self.home_location_x or not self.home_location_y or not self.home_location_z:
             self.w.btn_go_home.setEnabled(False)
@@ -147,6 +146,28 @@ class HandlerClass:
             self.w.lbl_home_x.setText(self.home_location_x)
             self.w.lbl_home_y.setText(self.home_location_y)
             self.w.lbl_no_home.hide()
+        # laser offsets
+        if not self.laser_offset_x or not self.laser_offset_y:
+            self.w.btn_ref_laser.setEnabled(False)
+            self.w.btn_laser_on.setEnabled(False)
+            self.w.groupBox_laser.hide()
+            self.w.lbl_no_laser_offsets.show()
+            self.add_alarm("No valid laser offsets found")
+        else:
+            self.w.lbl_laser_x.setText(self.laser_offset_x)
+            self.w.lbl_laser_y.setText(self.laser_offset_y)
+            self.w.lbl_no_laser_offsets.hide()
+        # tool sensor location
+        if not self.tool_sensor_x or not self.tool_sensor_y:
+            self.w.chk_tool_sensor.hide()
+            self.w.btn_go_g30.setEnabled(False)
+            self.w.groupBox_sensor.hide()
+            self.w.lbl_no_toolsensor.show()
+            self.add_alarm("No valid tool sensor location found")
+        else:
+            self.w.lbl_sensor_x.setText(self.tool_sensor_x)
+            self.w.lbl_sensor_y.setText(self.tool_sensor_y)
+            self.w.lbl_no_toolsensor.hide()
 
     def processed_key_event__(self,receiver,event,is_pressed,key,code,shift,cntrl):
         # when typing in MDI, we don't want keybinding to call functions
@@ -231,6 +252,9 @@ class HandlerClass:
         sys = self.system_list[int(data)]
         self.w.actionbutton_rel.setText(sys)
 
+    def tool_in_spindle_changed(self, obj, data):
+        self.tool_in_spindle = data
+
     def file_loaded(self, obj, filename):
         if filename is not None:
             self.w.progressBar.setValue(0)
@@ -252,6 +276,9 @@ class HandlerClass:
         for i in map(str.lower, self.axis_list):
             self.w["dro_axis_{}".format(i)].setProperty('homed', True)
             self.w["dro_axis_{}".format(i)].setStyle(self.w["dro_axis_{}".format(i)].style())
+        if self.tool_in_spindle == 0 and self.w.checkBox_reload_tool.checkState():
+            command = "M61 Q{}".format(self.reload_tool)
+            ACTION.CALL_MDI_WAIT(command)
         if self.last_loaded_program and self.w.checkBox_reload_program.checkState():
             ACTION.OPEN_PROGRAM(self.last_loaded_program)
             self.w.filemanager.updateDirectoryView(self.last_loaded_program)
@@ -270,68 +297,52 @@ class HandlerClass:
     #######################
     # CALLBACKS FROM FORM #
     #######################
-    def tool_widgets_enable(self,state):
-        self.w.btn_laser_on.setEnabled(state)
-        self.w.btn_gas_on.setEnabled(state)
-        self.w.btn_laser_en.setEnabled(state)
-        self.w.btn_light_on.setEnabled(state)
-        self.w.btn_powder_on.setEnabled(state)
-        self.w.comboBox_powder_num.setEnabled(state)
-
     # program frame
     def btn_start_clicked(self):
         if not STAT.is_auto_mode():
             return
         self.w.btn_from_line.setChecked(False)
-        self.tool_widgets_enable(False)
         self.add_alarm("Started program from line {}".format(self.start_line))
         ACTION.RUN(self.start_line)
-
-    def btn_abort_clicked(self):
-        self.tool_widgets_enable(True)
 
     def btn_reload_file_clicked(self):
         if self.last_loaded_program:
             self.w.progressBar.setValue(0)
             ACTION.OPEN_PROGRAM(self.last_loaded_program)
 
+    # tool frame
+    def btn_go_home_clicked(self):
+        ACTION.CALL_MDI_WAIT("G53 G0 Z0")
+        command = "G53 G0 X{} Y{}".format(self.w.lbl_home_x.text(), self.w.lbl_home_y.text())
+        ACTION.CALL_MDI_WAIT(command)
+
+    def btn_go_g30_clicked(self):
+        ACTION.CALL_MDI_WAIT("G53 G0 Z0")
+        command = "G53 G0 X{} Y{}".format(self.w.lbl_sensor_x.text(), self.w.lbl_sensor_y.text())
+        ACTION.CALL_MDI_WAIT(command)
+
+    def btn_ref_laser_clicked(self):
+        command = "G10 L20 P0 X{} Y{}".format(self.w.lbl_laser_x.text(), self.w.lbl_laser_y.text())
+        ACTION.CALL_MDI_WAIT(command)
+
     def btn_from_line_clicked(self, state):
         if state is False:
             self.start_line = 0
             self.w.btn_start.setText("START FROM 0")
 
-    # tool slot
-    def btn_laser_on_clicked(self,state):
-        if not STAT.is_mdi_mode():
+    def btn_touchoff_clicked(self):
+        code = None
+        if self.tool_in_spindle == 0:
+            self.add_alarm("Cannot probe with no tool loaded")
             return
-        if state is False:
-            ACTION.CALL_MDI_WAIT("M115")
-        else:
-            ACTION.CALL_MDI_WAIT("M105 P{}".format(self.w.slider_power.value()))
-
-    def btn_laser_enable_clicked(self,state):
-        if state is False:
-            ACTION.CALL_MDI_WAIT("M114")
-        else:
-            ACTION.CALL_MDI_WAIT("M104")
-
-    def btn_gas_on_clicked(self,state):
-        if state is False:
-            ACTION.CALL_MDI_WAIT("M116")
-        else:
-            ACTION.CALL_MDI_WAIT("M106")
-
-    def btn_light_on_clicked(self,state):
-        if state is False:
-            ACTION.CALL_MDI_WAIT("M117")
-        else:
-            ACTION.CALL_MDI_WAIT("M107")
-
-    def btn_powder_on_clicked(self,state):
-        if state is False:
-            ACTION.CALL_MDI_WAIT("M118 P{}".format(self.w.comboBox_powder_num.currentIndex()+1))
-        else:
-            ACTION.CALL_MDI_WAIT("M108 P{} Q{}".format(self.w.comboBox_powder_num.currentIndex()+1, float(self.w.slider_powder_vel.value())/10))
+        if self.w.btn_touchoff.text() == "TOOL\nSENSOR":
+            self.hal['zero_height'] = self.w.lineEdit_zero_height.text().encode('utf-8')
+            code = "o< tool_sensor > call"
+        elif self.w.btn_touchoff.text() == "TOUCH\nPLATE":
+            code = "o< touch_plate > call"
+        if not code is None:
+            ACTION.CALL_OWORD(code)
+            STAT.emit('forced-update')
 
     # override frame
     def btn_slow_clicked(self, state):
@@ -349,27 +360,6 @@ class HandlerClass:
     def btn_maxv_max_clicked(self):
         self.w.slider_maxv.setValue(self.max_velocity * 60)
 
-    def btn_power_fix_1(self):
-        self.w.slider_power.setValue(int(self.w.btn_power_1.text()))
-
-    def btn_power_fix_2(self):
-        self.w.slider_power.setValue(int(self.w.btn_power_2.text()))
-
-    def btn_power_fix_3(self):
-        self.w.slider_power.setValue(int(self.w.btn_power_3.text()))
-
-    def btn_powder_vel_fix_1(self):
-        self.w.slider_powder_vel.setValue(float(self.w.btn_powder_vel_1.text())*10)
-
-    def btn_powder_vel_fix_2(self):
-        self.w.slider_powder_vel.setValue(float(self.w.btn_powder_vel_2.text())*10)
-
-    def btn_powder_vel_fix_3(self):
-        self.w.slider_powder_vel.setValue(float(self.w.btn_powder_vel_3.text())*10)
-
-    def sld_powder_vel_changed(self,vel):
-        self.w.label_powder_vel.setText(str(float(vel)/10))
-
     # file tab
     def btn_gcode_edit_clicked(self, state):
         if not STAT.is_on_and_idle():
@@ -385,6 +375,16 @@ class HandlerClass:
             self.w.gcode_editor.hide()
             self.w.gcode_editor.readOnlyMode()
 
+    # tool tab
+    def btn_m61_clicked(self):
+        checked = self.w.tooloffsetview.get_checked_list()
+        if len(checked) > 1:
+            self.add_alarm("Select only 1 tool to load")
+        elif checked:
+            ACTION.CALL_MDI_WAIT("M61 Q{}".format(checked[0]))
+        else:
+            self.add_alarm("No tool selected")
+
     # alarm tab
     def btn_clear_alarms_clicked(self):
         STAT.emit('update-machine-log', None, 'DELETE')
@@ -395,6 +395,13 @@ class HandlerClass:
         filename = 'alarms_' + filename.replace(' ','_') + '.txt'
         with open(filename, 'w') as f:
             f.write(text)
+
+    # settings tab
+    def chk_tool_sensor(self, state):
+        if state:
+            self.w.btn_touchoff.setText("TOOL\nSENSOR")
+        else:
+            self.w.btn_touchoff.setText("TOUCH\nPLATE")
 
     def chk_override_limits(self, state):
         if state:
@@ -485,6 +492,9 @@ class HandlerClass:
     def closing_cleanup__(self):
         if self.w.PREFS_:
             self.w.PREFS_.putpref('Reload program', self.w.checkBox_reload_program.checkState(), bool, 'CUSTOM_FORM_ENTRIES')
+            self.w.PREFS_.putpref('Tool to load', self.tool_in_spindle, int, 'CUSTOM_FORM_ENTRIES')
+            self.w.PREFS_.putpref('Reload tool', self.w.checkBox_reload_tool.checkState(), bool, 'CUSTOM_FORM_ENTRIES')
+            self.w.PREFS_.putpref('Tool sensor', self.w.checkBox_tool_sensor.checkState(), bool, 'CUSTOM_FORM_ENTRIES')
 
     ##############################
     # required class boiler code #
